e4aa85f51939fa4e2982d7b191e1b5a62f880065
bigwhaleking <93432485+bigwhaleking@users.noreply.github.com>
2025-03-19 20:29:30 +0900

    Create Form1.cs

---
+++ 사용자 프로그램3/Form1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace detected_malware
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool mouseDown;
+        Point lastlocation;
+        private void lbl_title_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastlocation = e.Location;
+        }
+        private void lbl_title_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void lbl_title_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastlocation.X) + e.X,
+                    (this.Location.Y - lastlocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void btn_min_Click(object sender, MouseEventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void btn_max_Click(object sender, MouseEventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_close_Click(object sender, MouseEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lbl_title_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
