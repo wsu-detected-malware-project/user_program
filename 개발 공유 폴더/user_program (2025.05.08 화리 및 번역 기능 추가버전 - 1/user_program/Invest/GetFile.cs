eebe34708658dafa00f42c159a51e258399d1ac8
pth8174 <pth8174@naver.com>
2025-05-08 15:57:28 +0900

    Add files via upload

---
+++ 개발 공유 폴더/user_program (2025.05.08 화리 및 번역 기능 추가버전 - 1/user_program/Invest/GetFile.cs
@@ -0,0 +1,30 @@
+namespace user_program.Invest {
+    public class GetFile{
+            public static IEnumerable<string> GetFilesSafely(string rootDirectory, string[] searchPatterns) {
+            Stack<string> directories = new Stack<string>();
+            directories.Push(rootDirectory);
+
+            while (directories.Count > 0) {
+                string currentDir = directories.Pop();
+
+                try {
+                    foreach (string subDir in Directory.GetDirectories(currentDir)) {
+                        directories.Push(subDir);
+                    }
+                }
+                catch { continue; }
+
+                foreach (string pattern in searchPatterns) {
+                    string[] files = Array.Empty<string>();
+
+                    try { files = Directory.GetFiles(currentDir, pattern); }
+                    catch { continue; }
+
+                    foreach (string file in files) {
+                        yield return file;
+                    }
+                }
+            }
+        }
+    }
+}
